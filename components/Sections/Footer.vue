badcfe7f0b132fc7b5451527793ff42a0ff39b4e
SuguruOoki <oki.work0527@gmail.com>
2019-11-23 00:51:40 +0900

    Fix Footer

--- components/Sections/Footer.vue
+++ components/Sections/Footer.vue
@@ -2,12 +2,64 @@
   <footer>
     <div class="footer__info">
       <div class="container footer__info-container">
-        <div class="footer__copyright">
-          © {{ year }} — <span v-html="$t('footer.credits')"/>
+        <div class="flex-column">
+          <div class="footer__navi">
+            About
+          </div>
+          <div class="footer__navi">
+            Intro
+          </div>
+          <div class="footer__navi">
+            Service
+          </div>
+          <div class="footer__navi">
+            Columns
+          </div>
+          <div class="footer__navi">
+            Recruit
+          </div>
+          <div class="footer__navi">
+            Contact
+          </div>
         </div>
-        <a href="#top" class="footer__top-page">
-          ^
-        </a>
+        <div class="flex-column">
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+          </div>
+          <div class="footer__navi">
+            <button>一覧</button>
+          </div>
+        </div>
+        <div class="flex-column">
+          <div class="footer__navi">
+            株式会社ディチャーム
+          </div>
+          <div class="footer__navi">
+            〒141-0031<br/>
+            住所サンプル
+          </div>
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+            ニュース記事
+          </div>
+          <div class="footer__navi">
+          </div>
+          <div class="footer__navi">
+          </div>
+        </div>
       </div>
     </div>
   </footer>
@@ -23,6 +75,11 @@ export default {
 </script>
 
 <style lang="scss">
+.flex-column {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
 .footer {
 	&__info {
 		background-color: $background-primary;
